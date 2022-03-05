43ce62daccc0755042b0829d171a9be08a617b67
CNHsu <100649657+CNHsu@users.noreply.github.com>
2022-03-05 22:57:41 +0800

    swift-test

---
+++ test/test/test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test
+//
+//  Created by Sean on 2017/3/4.
+//  Copyright © 2017年 Sean. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test/test2/test2/test2/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  test2
+//
+//  Created by Sean on 2017/3/7.
+//  Copyright © 2017年 Sean. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+let possibleNumber = "123"
+let convertedNumber = possibleNumber.toInt()
+
